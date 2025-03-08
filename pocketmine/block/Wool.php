ba031d7ecee4f411a696c4fac7fa5ffff6344acc
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-03-08 17:44:33 +1000

    Update Wool.php

--- pocketmine/block/Wool.php
+++ pocketmine/block/Wool.php
@@ -17,50 +17,22 @@
  * @link http://www.pocketmine.net/
  *
  *
-*/
+ */
 
 declare(strict_types=1);
 
 namespace pocketmine\block;
 
-use pocketmine\item\Tool;
+use pocketmine\block\utils\ColoredTrait;
 
-class Wool extends Solid{
+class Wool extends Opaque{
+	use ColoredTrait;
 
-	protected $id = self::WOOL;
-
-	public function __construct($meta = 0){
-		$this->meta = $meta;
+	public function getFlameEncouragement() : int{
+		return 30;
 	}
 
-	public function getHardness(){
-		return 0.8;
+	public function getFlammability() : int{
+		return 60;
 	}
-
-	public function getToolType(){
-		return Tool::TYPE_SHEARS;
-	}
-
-	public function getName(){
-		static $names = [
-			0 => "White Wool",
-			1 => "Orange Wool",
-			2 => "Magenta Wool",
-			3 => "Light Blue Wool",
-			4 => "Yellow Wool",
-			5 => "Lime Wool",
-			6 => "Pink Wool",
-			7 => "Gray Wool",
-			8 => "Light Gray Wool",
-			9 => "Cyan Wool",
-			10 => "Purple Wool",
-			11 => "Blue Wool",
-			12 => "Brown Wool",
-			13 => "Green Wool",
-			14 => "Red Wool",
-			15 => "Black Wool",
-		];
-		return $names[$this->meta & 0x0f];
-	}
-
 }
