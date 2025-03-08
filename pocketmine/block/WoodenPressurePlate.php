590cd6b4298918757a207984bb80b67a5bdac841
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-03-08 17:48:46 +1000

    Update WoodenPressurePlate.php

--- pocketmine/block/WoodenPressurePlate.php
+++ pocketmine/block/WoodenPressurePlate.php
@@ -17,21 +17,30 @@
  * @link http://www.pocketmine.net/
  *
  *
-*/
+ */
 
 declare(strict_types=1);
 
 namespace pocketmine\block;
 
-class WoodenPressurePlate extends StonePressurePlate{
+use pocketmine\block\utils\WoodType;
+use pocketmine\block\utils\WoodTypeTrait;
 
-	protected $id = self::WOODEN_PRESSURE_PLATE;
+class WoodenPressurePlate extends SimplePressurePlate{
+	use WoodTypeTrait;
 
-	public function __construct($meta = 0){
-		$this->meta = $meta;
+	public function __construct(
+		BlockIdentifier $idInfo,
+		string $name,
+		BlockTypeInfo $typeInfo,
+		WoodType $woodType,
+		int $deactivationDelayTicks = 20 //TODO: make this mandatory in PM6
+	){
+		$this->woodType = $woodType;
+		parent::__construct($idInfo, $name, $typeInfo, $deactivationDelayTicks);
 	}
 
-	public function getName(){
-		return "Wooden Pressure Plate";
+	public function getFuelTime() : int{
+		return 300;
 	}
 }
